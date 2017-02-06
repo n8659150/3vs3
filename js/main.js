12d4ef94c4b0818b3a50547e7e3866e6563e1835
n8659150 <jackli900421@hotmail.com>
2017-02-06 14:50:20 +0800

    完成"篮球学院"部分的数据载入

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ avalon.ready(function(){
 		navData:["热门","新闻","公告","活动","赛事"],
 		getData:function(source){
 			switch(source){
+				// 0 == “热门”栏的下标
 				case 0:
 					fetch("data/news-center/hot-news.json")
 						.then(function(response){
@@ -21,6 +22,7 @@ avalon.ready(function(){
 							console.log("oops,error happens:",error);
 						})
 					break;
+				// 1 == “新闻”栏的下标
 				case 1:
 					fetch("data/news-center/latest-news.json")
 						.then(function(response){
@@ -34,6 +36,7 @@ avalon.ready(function(){
 							console.log("oops,error happens:",error);
 						})
 					break;
+				// 2 == “公告”栏的下标
 				case 2:
 					fetch("data/news-center/announcement-news.json")
 						.then(function(response){
@@ -47,6 +50,7 @@ avalon.ready(function(){
 							console.log("oops,error happens:",error);
 						})
 					break;
+				//3 == “活动”栏的下标 
 				case 3:
 					fetch("data/news-center/event-news.json")
 						.then(function(response){
@@ -60,6 +64,7 @@ avalon.ready(function(){
 							console.log("oops,error happens:",error);
 						})
 					break;
+				//4 == “赛事”栏的下标
 				case 4:
 					fetch("data/news-center/sportsgame-news.json")
 						.then(function(response){
@@ -89,7 +94,98 @@ avalon.ready(function(){
 			}
 				
 		}
-	})	
+	})
+	let basSchoolData = avalon.define({
+		$id:"basSchoolData",
+		data:[]
+		});	
+	let basSchoolNav = avalon.define({
+		$id:"basSchoolNav",
+		data:["最新","球员","新手","同人","官方"],
+		getData:function(source){
+			switch(source){
+				// 0 == “最新”栏的下标
+				case 0:
+					fetch("data/bas-school/latest-news.json")
+					.then(function(response){
+						return response.json();
+					})
+					.then(function(json){
+						basSchoolData.data = json;
+					})
+					.catch(function(error){
+						console.log("oops,error happens:",error);
+					})
+					break;
+				// 1 == “球员”栏的下标 
+				case 1:
+					fetch("data/bas-school/player-news.json")
+					.then(function(response){
+						return response.json();
+					})
+					.then(function(json){
+						basSchoolData.data = json;
+					})
+					.catch(function(error){
+						console.log("oops,error happens:",error);
+					})
+					break;
+				// 2 == “新手”栏的下标
+				case 2:
+					fetch("data/bas-school/beginner-news.json")
+					.then(function(response){
+						return response.json();
+					})
+					.then(function(json){
+						basSchoolData.data = json;
+					})
+					.catch(function(error){
+						console.log("oops,error happens:",error);
+					})
+					break;
+				// 3 == “同人”栏的下标
+				case 3:
+					fetch("data/bas-school/storybook-news.json")
+					.then(function(response){
+						return response.json();
+					})
+					.then(function(json){
+						basSchoolData.data = json;
+					})
+					.catch(function(error){
+						console.log("oops,error happens:",error);
+					})
+					break;
+				// 4 == “官方”栏的下标
+				case 4:
+					fetch("data/bas-school/official-news.json")
+					.then(function(response){
+						return response.json();
+					})
+					.then(function(json){
+						basSchoolData.data = json;
+					})
+					.catch(function(error){
+						console.log("oops,error happens:",error);
+					})
+					break;
+				default:
+					fetch("data/bas-school/official-news.json")
+						.then(function(response){
+							return response.json();
+						})
+						.then(function(json){
+							basSchoolData.data = json;
+						})
+						.catch(function(error){
+							console.log("oops,error happens:",error);
+						})
+					break;
+			}
+		}
+	})
+	
 		avalon.scan(document.body);
 		newsCenterData.data = newsCenterNav.getData(0);
+		basSchoolData.data = basSchoolNav.getData(0);
 	})
